2418708a54a05b23bc05d6745489bf54f35fb05f
Youssef Aouadni <aouadniyoussef01@gmail.com>
2024-12-17 19:35:58 +0100

    update

--- src/main/java/com/reachabl/models/meetings/AbstractRecurringMeeting.java
+++ src/main/java/com/reachabl/models/meetings/AbstractRecurringMeeting.java
@@ -1,8 +1,18 @@
 package com.reachabl.models.meetings;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import java.time.LocalDate;
 
-public class AbstractRecurringMeeting {
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "repeatType", visible = true)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = MonthlyByDayOfMonthRecurringMeeting.class, name = "MONTHLY_BY_DAY_OF_MONTH"),
+        @JsonSubTypes.Type(value = MonthlyByDayOfWeekRecurringMeeting.class, name = "MONTHLY_BY_DAY_OF_WEEK"),
+        @JsonSubTypes.Type(value = WeeklyRecurringMeeting.class, name = "WEEKLY"),
+        @JsonSubTypes.Type(value = DailyRecurringMeeting.class, name = "DAILY")
+})
+public abstract class AbstractRecurringMeeting {
     private RepeatType repeatType;
 
     private int repeatEvery;
@@ -16,6 +26,9 @@ public class AbstractRecurringMeeting {
         this.repeatEvery = repeatEvery;
         this.endDate = endDate;
         this.afterTime = afterTime;
+    }
+
+    public AbstractRecurringMeeting() {
     }
 
     public RepeatType getRepeatType() {
